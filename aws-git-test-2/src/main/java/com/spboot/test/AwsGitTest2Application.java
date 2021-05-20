30db55cec992a5ad6b05aa6395fedc04200d0670
장은선 <doma1854@naver.com>
2021-05-20 23:24:23 +0900

    1111

---
+++ aws-git-test-2/src/main/java/com/spboot/test/AwsGitTest2Application.java
@@ -0,0 +1,13 @@
+package com.spboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsGitTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsGitTest2Application.class, args);
+	}
+
+}
